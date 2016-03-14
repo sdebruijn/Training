38b8ac11c4d2b9b47ec951e10b35d4276b0b0935
Sijmen de Bruijn <sijmendeb@gmail.com>
2016-03-14 16:31:02 +0100

    Opgave 7

---
+++ HelloWorldDemo/src/nl/sijmen/firstprogram/Exercise7.java
@@ -0,0 +1,35 @@
+package nl.sijmen.firstprogram;
+
+public class Exercise7 {
+	static int x = 2;
+	static int y = 4;
+	
+	public static void main(String[] args) {
+
+		printResult();
+		if (x * y == 8) {
+			x = 1;
+		} else {
+			x = 2;
+		}
+		printResult();
+		if (x < y) {
+			x *= 2;
+		} else {
+			x++;
+		}
+		printResult();
+		if (x > y) {
+			x++;
+			y++;
+		} else {
+			x--;
+			y--;
+		}
+		printResult();
+	}
+	
+	public static void printResult() {
+		System.out.println("x: " + x + "   y: " + y);
+	}
+}
