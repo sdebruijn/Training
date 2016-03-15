4734f34e6edc1bfbd35f733369963d3889a5e561
Sijmen de Bruijn <sijmendeb@gmail.com>
2016-03-15 12:04:56 +0100

    Created testing file.

---
+++ HelloWorldDemo/src/nl/sijmen/training/Test.java
@@ -0,0 +1,17 @@
+package nl.sijmen.training;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		boolean myBool = false;
+		int yourInt = 10;
+		float hisFloat = 19.54f;
+		
+		System.out.println(hisFloat = yourInt);
+		System.out.println(yourInt > 10);
+		System.out.println(myBool = false);
+	}
+
+}
