bcc444e9c71ea41d75751bcd2f89edac5863e7e5
Sijmen de Bruijn <sijmendeb@gmail.com>
2016-03-16 16:51:42 +0100

    Opgave uit de sheets. aantalbytes( primitive type)  en  pastin( primitive type, primitive type) Generieke aanpak

---
+++ HelloWorldDemo/src/nl/sijmen/training/AantalBytes.java
@@ -0,0 +1,68 @@
+package nl.sijmen.training;
+
+
+class AantalBytes{
+	
+	public static void main (String[] args) {
+		long l = 12L;
+		int i = 12;
+		short sh = 12;
+		byte b = 1;
+		char c = 'c';
+		double d = 12.34;
+		float f = 12.45F;
+		boolean bool = true;
+		
+		String str = "blablaba";
+		
+		
+		System.out.println("Aantal bytes of boolean is " + aantalbytes(bool) );
+		System.out.println("Aantal bytes of float is   " + aantalbytes(f) );
+		System.out.println("Aantal bytes of double is  " + aantalbytes(d) );
+		System.out.println("Aantal bytes of byte is    " + aantalbytes(b) );
+		System.out.println("Aantal bytes of short is   " + aantalbytes(sh) );
+		System.out.println("Aantal bytes of int is     " + aantalbytes(i) );
+		System.out.println("Aantal bytes of long is    " + aantalbytes(l) );
+		System.out.println("Aantal bytes of char is    " + aantalbytes(c) );
+		try {
+			System.out.println("Aantal bytes of string is " + aantalbytes(str) );
+		} catch(IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		System.out.println("Char past in Long: " + pastin(c,l) );	
+		System.out.println("Long past in Char: " + pastin(l,c) );	
+		System.out.println("Int past in Float: " + pastin(i,f) );	
+		System.out.println("Long past in Double: " + pastin(l,d) );	
+	}
+
+	private static int aantalbytes(Object obj) {
+		String str = obj.getClass().getName();
+		switch ( str ) {
+			case "java.lang.Boolean":
+			case "java.lang.Byte":
+				return 1;
+			case "java.lang.Short":
+			case "java.lang.Character":
+				return 2;
+			case "java.lang.Integer":
+			case "java.lang.Float":
+				return 4;
+			case "java.lang.Double":
+			case "java.lang.Long":
+				return 8;
+			default:
+				throw new IllegalArgumentException("aantalbytes() does not accept an object type.");
+		}
+	}
+	
+	public static boolean pastin(Object a, Object b)
+	{
+		if (aantalbytes(a) > aantalbytes(b) ) { 
+			return false;
+		} else { 
+			return true;
+		}
+	}
+
+}
